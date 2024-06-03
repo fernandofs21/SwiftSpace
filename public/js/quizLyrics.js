95bcb9833dca79623e841580d71adf8fab5a8c5c
fernandofs21 <fernando.silva@sptech.school>
2024-06-03 15:02:05 -0300

    separação quizzes e inicio dashboard

--- public/js/quizLyrics.js
+++ public/js/quizLyrics.js
@@ -1,10 +1,4 @@
-var perguntas_quiz = [
-    {
-        pergunta: "Qual é a data de nascimento de Taylor Swift?",
-        alternativas: ["13 de Dezembro de 1989", "31 de Dezembro de 1989", "13 de Novembro de 1989", "31 de Novembro de 1989"],
-        resposta: 0,
-        dificuldade: "Fácil" 
-    }, 
+var perguntas_quiz = [ 
     {
         pergunta: "De qual música é a seguinte frase: 'When you think happiness, I hope you think that little black dress'",
         alternativas: ["Teardrops On My Guitar", "happiness", "Tim McGraw", "Crazier"],
@@ -12,34 +6,13 @@ var perguntas_quiz = [
         dificuldade: "Fácil"
     },
     {
-        pergunta: "Pra qual cidade Taylor e sua família se mudaram quando ela tinha 14 anos?",
-        alternativas: ["Pensilvânia", "Nova Iorque", "Nashville", "Los Angeles"],
-        resposta: 2,
-        dificuldade: "Difícil"
-    },
-    {
-        pergunta: "Quantos gatos Taylor tem?",
-        alternativas: ["1", "2", "3", "4"],
-        resposta: 2,
-        dificuldade: "Média"
-    },
-    {
-        pergunta: "Qual é o nome do terceiro álbum de estudio de Taylor?",
-        alternativas: ["Fearless", "Speak Now", "Red", "1989"],
-        resposta: 1,
-        dificuldade: "Fácil"
-    }, 
-    {
         pergunta: "Complete a letra: 'Is this the end of all the endings? My broken bones are ...'",
         alternativas: ["Fading", "Hurting", "Dazzling", "Mending"],
         resposta: 3,
         dificuldade: "Média"
     },
     {
-        pergunta: "Qual é o álbum mais longo de Taylor?",
-        alternativas: ["Red (Taylor's Version)", "The Tortured Poets Department: The Anthology", "Speak Now (Taylor's Version)", "Midnights (The Til Dawn Edition)"],
-        resposta: 0,
-        dificuldade: "Média"
+        pergunta: ""
     }
 ];
 
@@ -104,6 +77,8 @@ function verificar(selected) {
             selectedElement.classList.remove('correto');
             selectedElement.classList.remove('errado');
             corretaElement.classList.remove('correto');
+
+            
             pontos = 0
             embaralhar()
         }
