c83a87c6503545c2078e421fed9ee114525c7767
fernandofs21 <fernando.silva@sptech.school>
2024-06-11 17:18:33 -0300

    alteração select do ranking

--- src/models/rankingModel.js
+++ src/models/rankingModel.js
@@ -6,7 +6,7 @@ function gerarRanking(idQuiz) {
     var instrucaoSql = `
         SELECT u.nome, t1.pontuacao, DATE_FORMAT(t1.dtHora, "%d-%m-%y %H:%m:%S") as dia FROM tentativa t1 JOIN usuario u ON t1.fkUsuario = u.idUsuario WHERE t1.fkQuiz = ${idQuiz} 
         AND t1.pontuacao = (SELECT MAX(t2.pontuacao) FROM tentativa t2 WHERE t2.fkUsuario = t1.fkUsuario AND t2.fkQuiz = ${idQuiz})
-        AND t1.dtHora = (SELECT MAX(t3.dtHora) FROM tentativa t3 WHERE t3.fkUsuario = t1.fkUsuario AND t3.fkQuiz = ${idQuiz} AND t3.pontuacao = t1.pontuacao);
+        AND t1.dtHora = (SELECT MAX(t3.dtHora) FROM tentativa t3 WHERE t3.fkUsuario = t1.fkUsuario AND t3.fkQuiz = ${idQuiz} AND t3.pontuacao = t1.pontuacao) order by t1.pontuacao desc limit 7;
     `;
     console.log("Executando a instrução SQL: \n" + instrucaoSql);
     return database.executar(instrucaoSql);
